891cb219f46a8ebe8435fc91b01fcccefb1ca267
fedimaraquino <fedimaraquino@gmail.com>
2025-04-29 07:45:46 -0300

    Atualização do requirements.txt, .gitignore e criação de backup

--- importacao/management/commands/importar_dados.py
+++ importacao/management/commands/importar_dados.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 from django.core.management.base import BaseCommand
-from importacao.models import Seção, CDD, CDU, Cutter
+from importacao.models import Secao, CDD, CDU, Cutter
 
 class Command(BaseCommand):
     help = 'Importa dados das planilhas para o banco de dados'
@@ -15,9 +15,8 @@ class Command(BaseCommand):
             self.stdout.write('Importando seções...')
             df_secoes = pd.read_excel(os.path.join(planilhas_dir, 'secoes.xls'))
             for _, row in df_secoes.iterrows():
-                Seção.objects.get_or_create(
-                    código=row['código'],
-                    defaults={'descrição': row['descrição']}
+                Secao.objects.get_or_create(
+                    nome=row['nome']
                 )
 
             # Importar CDD
@@ -25,8 +24,8 @@ class Command(BaseCommand):
             df_cdd = pd.read_excel(os.path.join(planilhas_dir, 'cdd.xls'))
             for _, row in df_cdd.iterrows():
                 CDD.objects.get_or_create(
-                    código=row['código'],
-                    defaults={'descrição': row['descrição']}
+                    codigo=row['codigo'],
+                    defaults={'descricao': row['descricao']}
                 )
 
             # Importar CDU
@@ -34,8 +33,8 @@ class Command(BaseCommand):
             df_cdu = pd.read_excel(os.path.join(planilhas_dir, 'cdu.xls'))
             for _, row in df_cdu.iterrows():
                 CDU.objects.get_or_create(
-                    código=row['código'],
-                    defaults={'descrição': row['descrição']}
+                    codigo=row['codigo'],
+                    defaults={'descricao': row['descricao']}
                 )
 
             # Importar Cutter
@@ -43,8 +42,8 @@ class Command(BaseCommand):
             df_cutter = pd.read_excel(os.path.join(planilhas_dir, 'cutter.xls'))
             for _, row in df_cutter.iterrows():
                 Cutter.objects.get_or_create(
-                    código=row['código'],
-                    defaults={'descrição': row['descrição']}
+                    codigo=row['codigo'],
+                    defaults={'descricao': row['descricao']}
                 )
 
             self.stdout.write(self.style.SUCCESS('Importação concluída com sucesso!'))
